dd158827800ff61001bbade6496b4917946c4ddf
April Matus <apmartus8@gmail.com>
2019-11-12 09:48:42 -0700

    Starting to practice

---
+++ app.js
@@ -0,0 +1,9 @@
+// alert("Now in my own file!");
+
+document.body.style.background = "red";
+
+setInterval(() => {
+    // document.body.style.background = "white";
+    console.log("Hi!")
+
+}, 2000)
---
+++ practice/practice.js
@@ -0,0 +1,55 @@
+function ready() {
+    console.log("page ready");
+
+    let favs = document.forms.favs;
+    let colorEl = favs.element.color;
+
+    ColorEl.onchange = function() {
+        let el = document.createElement('p');
+        el.innerHTML = "You typed " + colorEl.value;
+
+        today.append(el);
+    };
+
+    go.onclick = function() {
+        alert("Please don't go!");
+    }
+}
+
+document.addEvent addEventListener("DOMContentLoaded", ready);
+
+
+cs.innerHTML = "Changed";
+
+cs.innerHTML = "<p>Now a paragraph</p>";
+
+cs.append("Now");
+
+// setTimeout(() => {
+//     cs.append("Latter");
+// }, 2000);
+
+// let x = 0;
+
+// setInterval(() => {
+//     empty.innerHTML = x + " seconds";
+//     x++;
+//     if (x > 5) {
+//         cs.innerHTML = "Done counting";
+//     } else {
+//         cs.innerHTML = "Not done counting";
+//     }
+// }, 1000);
+
+// let doSomething = function(word) {
+//     cs.innerHTML = ("saying" + word);
+// };
+
+// doSomething(hi);
+
+// let list = [1, 2, 3];
+
+// let i = 00
+// while (i < list.length) {
+//     console.log("logging " + list[i])
+// }
